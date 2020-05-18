6a35813cdff81cdfc9e7fb20e8399944580acbf9
HP-PC <HP-PC@DESKTOP-J8MUO76>
2020-05-18 22:24:22 +0530

    final

--- Online_Movie/src/main/java/com/capgemini/online_movie/service/Impl/CustomerServiceImpl.java
+++ Online_Movie/src/main/java/com/capgemini/online_movie/service/Impl/CustomerServiceImpl.java
@@ -1,5 +1,35 @@
 package com.capgemini.online_movie.service.Impl;
 
-public class CustomerServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capgemini.online_movie.dao.CustomerDao;
+import com.capgemini.online_movie.entity.Theater;
+import com.capgemini.online_movie.entity.Ticket;
+import com.capgemini.online_movie.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerDao customerdao; 
+
+	@Override
+	public Boolean chooseCity(String theaterCity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean bookMovieTicket(Theater theater) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean cancelMovieTicket(Ticket ticket) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
